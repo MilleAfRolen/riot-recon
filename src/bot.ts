cd126e028cb8619419f114f39bb66eb3cff45519
Mille <mille.af.rolen@gmail.com>
2025-02-02 20:30:30 +0100

    feat: added ping command, event handling and validation

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,37 @@
 import "dotenv/config";
-import { Client } from "discord.js";
+import { Client, Collection, Events, MessageFlags } from "discord.js";
+import fs from "fs";
+import path from "path";
+import { CommandKit } from "commandkit";
+// import {
+//   getPUUID,
+//   getSummonerInfo,
+//   getCurrentDayMatchIds,
+//   getMatchInfo,
+// } from "./api";
 
 const client = new Client({
-  intents: ["Guilds", "GuildMessages", "GuildMembers", "MessageContent"],
+  intents: ["Guilds", "GuildMessages", "DirectMessages"],
 });
 
-client.on("ready", (c) => {
-  console.log(`${c.user.username} is online`);
+new CommandKit({
+  client,
+  devGuildIds: ["1334996004432384110"],
+  devUserIds: ["351452531588726786", "222990126370914306"],
+  eventsPath: `${__dirname}/events`,
+  commandsPath: `${__dirname}/commands`,
+  validationsPath: `${__dirname}/validations`,
+  bulkRegister: true,
+  skipBuiltInValidations: true,
 });
 
 client.login(process.env.TOKEN);
+
+// const league = async () => {
+//   const puuid = await getPUUID("richtern", "euw");
+//   const summonerId = await getSummonerInfo(puuid);
+//   const test = await getCurrentDayMatchIds(puuid);
+//   const test2 = await getMatchInfo(test[0]);
+// };
+
+// league();
